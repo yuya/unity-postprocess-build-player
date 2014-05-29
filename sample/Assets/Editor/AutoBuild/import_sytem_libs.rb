3735c9fe84e409a24925b9f311e79c5f74f4e2cb
yuya <twilightwind@gmail.com>
2014-05-29 22:16:26 +0900

    frameworks, libs をインポートして attr を設定するところまで

--- sample/Assets/Editor/AutoBuild/import_sytem_libs.rb
+++ sample/Assets/Editor/AutoBuild/import_sytem_libs.rb
@@ -5,13 +5,57 @@ require "rubygems"
 require "pathname"
 require "xcodeproj"
 
-# proj_path = ARGV[0]
-# build_env = ARGV[1]
+# project_path = ARGV[0]
+# build_env    = ARGV[1]
+proj_path  = "/Users/yuya/Workspace/unity-postprocess-build-player/tmp/iOS/Unity-iPhone.xcodeproj"
+build_env  = "Product"
+libraries  = [
+  "sqlite3"
+]
+framework_list = [
+  "AdSupport",
+  "Security",
+  "CoreTelephony",
+  "StoreKit",
+  "Security",
+  "CoreText",
+  "MessageUI",
+  "Twitter"
+]
 
-path = ARGV[0]
+@project     = Xcodeproj::Project.open proj_path
+@build_phase = @project.objects.find { |obj| obj.is_a? Xcodeproj::Project::PBXFrameworksBuildPhase }
 
-File.open("#{path}/hoge.txt", "w") {|f|
-  f.write "HOGE"
-}
+def import_system_frameworks(framework_list)
+  @project.targets.each do |target|
+    if target.name == "Unity-iPhone-simulator" then
+      next
+    end
 
+    frameworks = target.add_system_framework framework_list
+  end
+end
 
+def set_framework_attributes(framework_list, option = :optional)
+  @project.targets.each do |target|
+    if target.name == "Unity-iPhone-simulator" then
+      next
+    end
+
+    settings = { "ATTRIBUTES" => (option == :optional) ? ["Weak"] : ["Strong"] }
+
+    framework_list.each { |framework|
+      matched = @build_phase.files_references.find { |phase|
+        (phase.is_a? Xcodeproj::Project::Object::PBXFileReference) && (phase.name =~ /^#{framework}/)
+      }
+
+      matched.build_files[0].settings = settings
+    }
+  end
+end
+
+import_system_frameworks framework_list
+set_framework_attributes framework_list, :optional
+# set_atttributes fram
+
+@project.save

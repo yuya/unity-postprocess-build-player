1389147bbb3f5905fb2ba63fa9e52ff0b4721954
yuya <twilightwind@gmail.com>
2014-05-30 19:24:16 +0900

    同じ処理消した

--- sample/Assets/Editor/AutoBuild/import_sytem_libs.rb
+++ sample/Assets/Editor/AutoBuild/import_sytem_libs.rb
@@ -5,57 +5,57 @@ require "rubygems"
 require "pathname"
 require "xcodeproj"
 
-# project_path = ARGV[0]
-# build_env    = ARGV[1]
-proj_path  = "/Users/yuya/Workspace/unity-postprocess-build-player/tmp/iOS/Unity-iPhone.xcodeproj"
-build_env  = "Product"
-libraries  = [
+# proj_path = ARGV[0]
+# build_env = ARGV[1]
+proj_path = ARGV[0] ? ARGV[0] : "/Users/yuya/Workspace/unity-postprocess-build-player/tmp/iOS/Unity-iPhone.xcodeproj"
+build_env = ARGV[1] ? ARGV[1] : "Product"
+unity_ver = ARGV[2] ? ARGV[2] : "2.3.2"
+# proj_path  = "/Users/yuya/Workspace/unity-postprocess-build-player/tmp/iOS/Unity-iPhone.xcodeproj"
+# build_env  = "Product"
+
+File.write("/Users/yuya/Desktop/unity_ver.txt", unity_ver);
+
+libraries = [
   "sqlite3"
 ]
-framework_list = [
+frameworks = [
   "AdSupport",
   "Security",
-  "CoreTelephony",
-  "StoreKit",
-  "Security",
-  "CoreText",
-  "MessageUI",
-  "Twitter"
+  "CoreTelephony"
+]
+weak_frameworks = [
+  "UIKit",
+  "AdSupport"
 ]
 
 @project     = Xcodeproj::Project.open proj_path
+@target      = @project.targets.find { |target| target.name == "Unity-iPhone" }
 @build_phase = @project.objects.find { |obj| obj.is_a? Xcodeproj::Project::PBXFrameworksBuildPhase }
 
-def import_system_frameworks(framework_list)
-  @project.targets.each do |target|
-    if target.name == "Unity-iPhone-simulator" then
-      next
-    end
+unless @target then exit end
 
-    frameworks = target.add_system_framework framework_list
-  end
+def import_system_libraries(libraries)
+  @target.add_system_libraries libraries
 end
 
-def set_framework_attributes(framework_list, option = :optional)
-  @project.targets.each do |target|
-    if target.name == "Unity-iPhone-simulator" then
-      next
-    end
+def import_system_frameworks(frameworks)
+  @target.add_system_frameworks frameworks
+end
 
-    settings = { "ATTRIBUTES" => (option == :optional) ? ["Weak"] : ["Strong"] }
+def set_frameworks_attribute(weak_frameworks, option = :optional)
+  settings = { "ATTRIBUTES" => (option == :optional) ? ["Weak"] : ["Strong"] }
 
-    framework_list.each { |framework|
-      matched = @build_phase.files_references.find { |phase|
-        (phase.is_a? Xcodeproj::Project::Object::PBXFileReference) && (phase.name =~ /^#{framework}/)
-      }
-
-      matched.build_files[0].settings = settings
+  weak_frameworks.each { |framework|
+    matched = @build_phase.files_references.find { |file|
+      (file.is_a? Xcodeproj::Project::Object::PBXFileReference) && (file.name =~ /^#{framework}/)
     }
-  end
+
+    matched.build_files[0].settings = settings
+  }
 end
 
-import_system_frameworks framework_list
-set_framework_attributes framework_list, :optional
-# set_atttributes fram
+import_system_libraries  libraries
+import_system_frameworks frameworks
+set_frameworks_attribute weak_frameworks, :optional
 
 @project.save

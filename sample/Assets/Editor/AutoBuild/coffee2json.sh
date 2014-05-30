26ab7c77400409b7df4b08bdb8b25e5bb8e372ce
yuya <twilightwind@gmail.com>
2014-05-30 11:24:16 +0900

    add coffee2json

---
+++ sample/Assets/Editor/AutoBuild/coffee2json.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if ! type coffee > /dev/null 2>&1; then echo "Please install the coffee-script."; exit; fi
+if [[ ! "${1}" =~ \.coffee$ ]];    then echo "File extension error";              exit; fi
+
+echo \{
+  cat $1 | \
+  coffee --print --bare --compile --stdio | \
+  sed -e '1,2d; $d' | \
+  sed "s/\([^ '\"].*[^ '\"]\): /\"\1\": /"
+echo \}
